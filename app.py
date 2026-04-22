8da8719cdeda918afd6016817e9a963405edc3ba
SamsonKunchala <samsonkunchala297@gmail.com>
2026-04-22 20:11:25 +0530

    chnaged

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "mi chelle kuda odhu over chesthadhi mi amma ok naku love you saritha lechipodham dha!!!!"
+    return "Good job thokka ivvi odhu isthe mudhu ivvu intha kastapadda!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
