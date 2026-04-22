33ccfbd1763dad92c08fb96061538bdf649f58e8
Samsonkunchala <64310707+Samsonkunchala@users.noreply.github.com>
2026-04-22 23:10:47 +0530

    minor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Good job thokka ivvi odhu isthe mudhu ivvu intha kastapadda!!!"
+    return "This is next version of my code!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
